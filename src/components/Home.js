ccc375d855ba3dd38b8d49fb60df4bab887becef
unknown <samehrizk1993@gmail.com>
2024-12-30 18:07:18 +0200

    new update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./header/Header";
 import Numbers from "./numbers/Numbers";
+import Section from "./section-one/Section";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Numbers />
+      <Section />
     </>
   );
 };
---
+++ src/components/section-one/Section.js
@@ -0,0 +1,28 @@
+import React from "react";
+import food from "./../../assets/01.png";
+import "./section.css";
+
+const Section = () => {
+  return (
+    <div className="container sec-1">
+      <div className="row">
+        <div className="col-md-7">
+          <img src={food} title="img" />
+        </div>
+        <div className="col-md-5 text-left">
+          <h2>
+            We pride ourselves on making real food from the best ingredients.
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et
+            purus a odio finibus bibendum in sit amet leo. Mauris feugiat erat
+            tellus.
+          </p>
+          <button className="butn">Learn More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section;
