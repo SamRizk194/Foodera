73a998496c458c72b4e06a12cbae0dbb3f3bc198
unknown <samehrizk1993@gmail.com>
2025-01-05 03:21:08 +0200

    adding new photo

--- src/components/foot/Foot.js
+++ src/components/foot/Foot.js
@@ -3,9 +3,9 @@ import "./foot.css";
 
 const Foot = () => {
   return (
-    <div className="container-fluid pitza text-center ">
-      <div className="row">
-        <div className="col-md-8">
+    <div className="container-fluid background-container">
+      <div className="row overlay">
+        <div className="col-md-8 overlay-text">
           <h4>Baked fresh daily by bakers with passion.</h4>
         </div>
         <div className="col-md-4">
